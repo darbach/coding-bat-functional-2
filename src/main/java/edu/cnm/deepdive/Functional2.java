7145ba2b5a84209d35d9056f5542d1837d6133ab
darbach <danielarbach@gmail.com>
2021-04-13 09:58:17 -0600

    Completed Functional-2.

--- src/main/java/edu/cnm/deepdive/Functional2.java
+++ src/main/java/edu/cnm/deepdive/Functional2.java
@@ -7,17 +7,149 @@ public class Functional2 {
 
   /**
    * Given a list of integers, return a list of the integers, omitting any that are less than 0.
-   *
-   * noNeg([1, -2]) → [1]
-   * noNeg([-3, -3, 3, 3]) → [3, 3]
-   * noNeg([-1, -1, -1]) → []
+   * <p>
+   * noNeg([1, -2]) → [1] noNeg([-3, -3, 3, 3]) → [3, 3] noNeg([-1, -1, -1]) → []
    *
    * @param nums values to be operated upon
    * @return a list of values with 0s removed
    */
   public List<Integer> noNeg(List<Integer> nums) {
-    nums.removeIf((n) -> { return n < 0; });
+    nums.removeIf((n) -> {
+      return n < 0;
+    });
     return nums;
+  }
+
+  /**
+   * Given a list of non-negative integers, return a list of those numbers except omitting any that
+   * end with 9. (Note: % by 10)
+   *
+   * no9([1, 2, 19]) → [1, 2]
+   * no9([9, 19, 29, 3]) → [3]
+   * no9([1, 2, 3]) → [1, 2, 3]
+   *
+   * @param nums values to be operated upon
+   * @return all values ending with "9" removed
+   */
+  public List<Integer> no9(List<Integer> nums) {
+    nums.removeIf((n) -> n % 10 == 9);
+    return nums;
+  }
+
+  /**
+   * Given a list of integers, return a list of those numbers, omitting any that are between 13 and
+   * 19 inclusive.
+   *
+   * noTeen([12, 13, 19, 20]) → [12, 20]
+   * noTeen([1, 14, 1]) → [1, 1]
+   * noTeen([15]) → []
+   *
+   * @param nums values to be operated upon
+   * @return all teen numbers removed
+   */
+  public List<Integer> noTeen(List<Integer> nums) {
+    nums.removeIf((n) -> (n >= 13) && (n <= 19));
+    return nums;
+  }
+
+  /**
+   * Given a list of strings, return a list of the strings, omitting any string that contains a "z".
+   * (Note: the str.contains(x) method returns a boolean)
+   *
+   * noZ(["aaa", "bbb", "aza"]) → ["aaa", "bbb"]
+   * noZ(["hziz", "hzello", "hi"]) → ["hi"]
+   * noZ(["hello", "howz", "are", "youz"]) → ["hello", "are"]
+   *
+   * @param strings values to be operated upon
+   * @return strings that do not contain any "z"s
+   */
+  public List<String> noZ(List<String> strings) {
+    strings.removeIf((s) -> s.contains("z"));
+    return strings;
+  }
+
+  /**
+   * Given a list of strings, return a list of the strings, omitting any string length 4 or more.
+   *
+   * noLong(["this", "not", "too", "long"]) → ["not", "too"]
+   * noLong(["a", "bbb", "cccc"]) → ["a", "bbb"]
+   * noLong(["cccc", "cccc", "cccc"]) → []
+   *
+   * @param strings values to be operated upon
+   * @return strings that are shorter than 4 characters
+   */
+  public List<String> noLong(List<String> strings) {
+    strings.removeIf((s) -> s.length() >= 4);
+    return strings;
+  }
+
+  /**
+   * Given a list of strings, return a list of the strings, omitting any string length 3 or 4.
+   *
+   * no34(["a", "bb", "ccc"]) → ["a", "bb"]
+   * no34(["a", "bb", "ccc", "dddd"]) → ["a", "bb"]
+   * no34(["ccc", "dddd", "apple"]) → ["apple"]
+   *
+   * @param strings values to be operated upon
+   * @return strings that are not 3 or 4 characters long
+   */
+  public List<String> no34(List<String> strings) {
+    strings.removeIf((s) -> s.length() == 3 || s.length() == 4);
+    return strings;
+  }
+
+  /**
+   * Given a list of strings, return a list where each string has "y" added at its end, omitting any
+   * resulting strings that contain "yy" as a substring anywhere.
+   *
+   * noYY(["a", "b", "c"]) → ["ay", "by", "cy"]
+   * noYY(["a", "b", "cy"]) → ["ay", "by"]
+   * noYY(["xx", "ya", "zz"]) → ["xxy", "yay", "zzy"]
+   *
+   * @param strings values to be operated upon
+   * @return strings with "y" appended but omitted if they contain "yy"
+   */
+  public List<String> noYY(List<String> strings) {
+    return strings.stream()
+        .map((s) -> s + "y")
+        .filter((s) -> !s.contains("yy"))
+        .collect(Collectors.toList());
+  }
+
+  /**
+   * Given a list of non-negative integers, return a list of those numbers multiplied by 2, omitting
+   * any of the resulting numbers that end in 2.
+   *
+   * two2([1, 2, 3]) → [4, 6]
+   * two2([2, 6, 11]) → [4]
+   * two2([0]) → [0]
+   *
+   * @param nums values to be operated upon
+   * @return numbers multiplied by 2 and omitted if they end in 2
+   */
+  public List<Integer> two2(List<Integer> nums) {
+    return nums.stream()
+        .map((i) -> i * 2)
+        .filter((i) -> i % 10 != 2)
+        .collect(Collectors.toList());
+  }
+
+  /**
+   * Given a list of integers, return a list of those numbers squared and the product added to 10,
+   * omitting any of the resulting numbers that end in 5 or 6.
+   *
+   * square56([3, 1, 4]) → [19, 11]
+   * square56([1]) → [11]
+   * square56([2]) → [14]
+   *
+   * @param nums values to be operated upon
+   * @return numbers squared, added to 10, and ommitting any that end in 5 or 6
+   */
+  public List<Integer> square56(List<Integer> nums) {
+    return nums.stream()
+        .map((i) -> i * i + 10)
+        .filter((i) -> (i % 10 != 5) && (i % 10 != 6))
+        .collect(Collectors.toList());
   }
 
 }
--- src/test/java/edu/cnm/deepdive/Functional2Test.java
+++ src/test/java/edu/cnm/deepdive/Functional2Test.java
@@ -12,13 +12,69 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class Functional2Test {
 
-  Functional2 functional2 = new Functional2();
+  private static final Functional2 functional2 = new Functional2();
 
   @ParameterizedTest(name = "[{index}]Asserting noNeg({0}) == {1}.")
   @MethodSource({"noNeg"})
   void noNeg(List<Integer> nums, List<Integer> expected) {
     List<Integer> input = new ArrayList<>(nums);
     assertArrayEquals(expected.toArray(), functional2.noNeg(input).toArray());
+  }
+
+  @ParameterizedTest(name = "[{index}]Asserting no9({0}) == {1}.")
+  @MethodSource({"no9"})
+  void no9(List<Integer> nums, List<Integer> expected) {
+    List<Integer> input = new ArrayList<>(nums);
+    assertArrayEquals(expected.toArray(), functional2.no9(input).toArray());
+  }
+
+  @ParameterizedTest(name = "[{index}]Asserting noTeen({0}) == {1}.")
+  @MethodSource({"noTeen"})
+  void noTeen(List<Integer> nums, List<Integer> expected) {
+    List<Integer> input = new ArrayList<>(nums);
+    assertArrayEquals(expected.toArray(), functional2.noTeen(input).toArray());
+  }
+
+  @ParameterizedTest(name = "[{index}]Asserting noZ({0}) == {1}.")
+  @MethodSource({"noZ"})
+  void noZ(List<String> strings, List<String> expected) {
+    List<String> input = new ArrayList<>(strings);
+    assertArrayEquals(expected.toArray(), functional2.noZ(input).toArray());
+  }
+
+  @ParameterizedTest(name = "[{index}]Asserting noLong({0}) == {1}.")
+  @MethodSource({"noLong"})
+  void noLong(List<String> strings, List<String> expected) {
+    List<String> input = new ArrayList<>(strings);
+    assertArrayEquals(expected.toArray(), functional2.noLong(input).toArray());
+  }
+
+  @ParameterizedTest(name = "[{index}]Asserting no34({0}) == {1}.")
+  @MethodSource({"no34"})
+  void no34(List<String> strings, List<String> expected) {
+    List<String> input = new ArrayList<>(strings);
+    assertArrayEquals(expected.toArray(), functional2.no34(input).toArray());
+  }
+
+  @ParameterizedTest(name = "[{index}]Asserting noYY({0}) == {1}.")
+  @MethodSource({"noYY"})
+  void noYY(List<String> strings, List<String> expected) {
+    List<String> input = new ArrayList<>(strings);
+    assertArrayEquals(expected.toArray(), functional2.noYY(input).toArray());
+  }
+
+  @ParameterizedTest(name = "[{index}]Asserting two2({0}) == {1}.")
+  @MethodSource({"two2"})
+  void two2(List<Integer> strings, List<Integer> expected) {
+    List<Integer> input = new ArrayList<>(strings);
+    assertArrayEquals(expected.toArray(), functional2.two2(input).toArray());
+  }
+
+  @ParameterizedTest(name = "[{index}]Asserting square56({0}) == {1}.")
+  @MethodSource({"square56"})
+  void square56(List<Integer> strings, List<Integer> expected) {
+    List<Integer> input = new ArrayList<>(strings);
+    assertArrayEquals(expected.toArray(), functional2.square56(input).toArray());
   }
 
   static Stream<Arguments> noNeg() {
@@ -33,6 +89,100 @@ class Functional2Test {
         Arguments.of(Arrays.asList(0, 1, 2), List.of(0, 1, 2)),
         Arguments.of(Arrays.asList(3, -10, 1, -1, 4, -400), List.of(3, 1, 4)),
         Arguments.of(Arrays.asList(-1, 3, 1, -1, -10, -100, -111, 5), List.of(3, 1, 5))
+    );
+  }
+
+  static Stream<Arguments> no9() {
+    return Stream.of(
+        Arguments.of(Arrays.asList(1, 2, 19), List.of(1, 2)),
+        Arguments.of(Arrays.asList(9, 19, 29, 3), List.of(3)),
+        Arguments.of(Arrays.asList(1, 2, 3), List.of(1, 2, 3)),
+        Arguments.of(Arrays.asList(45, 99, 90, 28, 13, 999, 0), List.of(45, 90, 28, 13, 0)),
+        Arguments.of(Arrays.asList(), List.of()),
+        Arguments.of(Arrays.asList(9), List.of()),
+        Arguments.of(Arrays.asList(0, 9, 0), List.of(0, 0))
+    );
+  }
+
+  static Stream<Arguments> noZ() {
+    return Stream.of(
+        Arguments.of(List.of("aaa", "bbb", "aza"), List.of("aaa", "bbb")),
+        Arguments.of(List.of("hziz", "hzello", "hi"), List.of("hi")),
+        Arguments.of(List.of("hello", "howz", "are", "youz"), List.of("hello", "are")),
+        Arguments.of(List.of(), List.of()),
+        Arguments.of(List.of(""), List.of("")),
+        Arguments.of(List.of("x", "y", "z"), List.of("x", "y"))
+    );
+  }
+
+  static Stream<Arguments> noLong() {
+    return Stream.of(
+        Arguments.of(List.of("this", "not", "too", "long"), List.of("not", "too")),
+        Arguments.of(List.of("a", "bbb", "cccc"), List.of("a", "bbb")),
+        Arguments.of(List.of("cccc", "cccc", "cccc"), List.of()),
+        Arguments.of(List.of(), List.of()),
+        Arguments.of(List.of(""), List.of("")),
+        Arguments.of(List.of("empty", "", "empty"), List.of("")),
+        Arguments.of(List.of("a"), List.of("a")),
+        Arguments.of(List.of("aaaa", "bbb", "***", "333"), List.of("bbb", "***", "333"))
+    );
+  }
+
+  static Stream<Arguments> no34() {
+    return Stream.of(
+        Arguments.of(List.of("a", "bb", "ccc"), List.of("a", "bb")),
+        Arguments.of(List.of("a", "bb", "ccc", "dddd"), List.of("a", "bb")),
+        Arguments.of(List.of("ccc", "dddd", "apple"), List.of("apple")),
+        Arguments.of(List.of("this", "not", "too", "long"), List.of()),
+        Arguments.of(List.of("a", "bbb", "cccc", "xx"), List.of("a", "xx")),
+        Arguments.of(List.of("dddd", "ddd", "xxxxxxx"), List.of("xxxxxxx")),
+        Arguments.of(List.of(), List.of()),
+        Arguments.of(List.of(""), List.of("")),
+        Arguments.of(List.of("empty", "", "empty"), List.of("empty", "", "empty")),
+        Arguments.of(List.of("a"), List.of("a")),
+        Arguments.of(List.of("aaaa", "bbb", "*****", "333"), List.of("*****"))
+    );
+  }
+
+  static Stream<Arguments> noYY() {
+    return Stream.of(
+        Arguments.of(List.of("a", "b", "c"), List.of("ay", "by", "cy")),
+        Arguments.of(List.of("a", "b", "cy"), List.of("ay", "by")),
+        Arguments.of(List.of("xx", "ya", "zz"), List.of("xxy", "yay", "zzy")),
+        Arguments.of(List.of("xx", "yay", "zz"), List.of("xxy", "zzy")),
+        Arguments.of(List.of("yyx", "y", "zzz"), List.of("zzzy")),
+        Arguments.of(List.of("hello", "there"), List.of("helloy", "therey")),
+        Arguments.of(List.of("ya"), List.of("yay")),
+        Arguments.of(List.of(), List.of()),
+        Arguments.of(List.of(""), List.of("y")),
+        Arguments.of(List.of("xx", "yy", "zz"), List.of("xxy", "zzy"))
+    );
+  }
+
+  static Stream<Arguments> two2() {
+    return Stream.of(
+        Arguments.of(List.of(1, 2, 3), List.of(4, 6)),
+        Arguments.of(List.of(2, 6, 11), List.of(4)),
+        Arguments.of(List.of(0), List.of(0)),
+        Arguments.of(List.of(), List.of()),
+        Arguments.of(List.of(1, 11, 111, 16), List.of()),
+        Arguments.of(List.of(2, 3, 5, 7, 11), List.of(4, 6, 10, 14)),
+        Arguments.of(List.of(3, 1, 4, 1, 6, 99, 0), List.of(6, 8, 198, 0))
+    );
+  }
+
+  static Stream<Arguments> square56() {
+    return Stream.of(
+        Arguments.of(List.of(3, 1, 4), List.of(19, 11)),
+        Arguments.of(List.of(1), List.of(11)),
+        Arguments.of(List.of(2), List.of(14)),
+        Arguments.of(List.of(3), List.of(19)),
+        Arguments.of(List.of(4), List.of()),
+        Arguments.of(List.of(5), List.of()),
+        Arguments.of(List.of(6), List.of()),
+        Arguments.of(List.of(7), List.of(59)),
+        Arguments.of(List.of(1, 2, 3, 4, 5, 6, 7), List.of(11, 14, 19, 59)),
+        Arguments.of(List.of(3, -1, -4, 1, 5, 9), List.of(19, 11, 11, 91))
     );
   }
 
